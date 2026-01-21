cdbac1e37685c519972f162053282a814c1b0e6b
Jason Kuan <e1398909@u.nus.edu>
2026-01-22 06:43:09 +0800

    Adding add, list to Blitz Bot

--- src/main/java/Blitz.java
+++ src/main/java/Blitz.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Blitz {
     public static final String Line = "____________________________________________________________";
+    public static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
         
         System.out.println(Line);
         System.out.println(" Hello! I'm Blitz");
@@ -22,9 +25,29 @@ public class Blitz {
                 System.out.println(Line);
                 System.out.println("Then i can finally enjoy some break!");
                 System.out.println(Line);
-            } else {
+            } else {}
+            
+            if (input.equals("list")) {
                 System.out.println(Line);
-                System.out.println(" " + input);
+                if (taskCount == 0) {
+                    System.out.println("Currently no task ongoing");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(i + 1 + ". " + tasks[i]);
+                    }
+                }
+                System.out.println(Line);
+                continue;
+            }
+
+            if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = input;
+                taskCount += 1;
+                System.out.println(Line);
+                System.out.println("added: " + input);
+                System.out.println(Line);
+            } else {
+                System.out.println("I am currently sleeping, do not disturb me!");
                 System.out.println(Line);
             }
         }
