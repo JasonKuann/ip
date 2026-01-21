089ebb23919ce3cba7e7a3ad7d6422d2dc285799
Jason Kuan <e1398909@u.nus.edu>
2026-01-22 06:24:18 +0800

    Updating Blitz bot with Echo

--- src/main/java/Blitz.java
+++ src/main/java/Blitz.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Blitz {
+    public static final String Line = "____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println(Line);
         System.out.println(" Hello! I'm Blitz");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(Line);
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.equals("bye")) {
+                System.out.println(Line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(Line);
+                break;
+            } else if (input.equals("nothing")) {
+                System.out.println(Line);
+                System.out.println("Then i can finally enjoy some break!");
+                System.out.println(Line);
+            } else {
+                System.out.println(Line);
+                System.out.println(" " + input);
+                System.out.println(Line);
+            }
+        }
+        scanner.close();
     }
 }
